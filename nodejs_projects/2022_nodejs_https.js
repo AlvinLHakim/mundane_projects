153d8586bb480024e5cacfd9d9166ea7727e8a8b
AlvinLHakim <alvinlhakim@gmail.com>
2022-05-24 05:52:56 +0700

    Created a file named: 2022_nodejs_https.js

---
+++ nodejs_projects/2022_nodejs_https.js
@@ -0,0 +1,9 @@
+// use require() to include module
+//module = nodejs library
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
