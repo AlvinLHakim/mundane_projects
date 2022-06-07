b2b417cbcafa6c3b109d53174d8ff92fb729045a
AlvinLHakim <alvinlhakim@gmail.com>
2022-06-08 03:14:34 +0700

    some changes

--- web_projects/2022_form/script.js
+++ web_projects/2022_form/script.js
@@ -1,13 +1,13 @@
 const username = document.getElementById('username');
 const email = document.getElementById('email-address');
-const  submit = document.getElementById('submit-btn');
+const  submitBtn = document.getElementById('submit-btn');
 
 const validate = (e) => {
     e.preventDefault();
 
     // If username input is left empty, the form will be invalid
     if(username.value ===""){
-        document.getElementById('username').innerHTML = "Please insert your username.";
+        document.getElementById('user-error').innerHTML = "Please insert your username.";
         username.focus();
         return false;
     } 
@@ -25,4 +25,4 @@ const validate = (e) => {
 //     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 // }
 
-submit.addEventListener('click', validate);
+submitBtn.addEventListener('click', validate);
